c2369b81a696360799d538510f1843a88829c9ba
Kohei-Tsukamoto <kouhei0525@gmail.com>
2020-04-25 06:47:59 +0000

    php_13

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create','Admin\NewsController@add')->middleware('auth');
+Route::group(['prefix' => 'admin','middleware' => 'auth'], function() {
+    Route::get('news/create','Admin\NewsController@add');
+    Route::post('news/create', 'Admin\NewsController@create');
+    Route::post('profile/create','Admin\ProfileController@create');
+    Route::post('profile/edit','Admin\ProfileController@update');
 });
 
 //課題３
