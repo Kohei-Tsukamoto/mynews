8855e9fdb53d44b6b5300e81ddcbc685c3632e44
Kohei-Tsukamoto <kouhei0525@gmail.com>
2020-04-30 07:04:48 +0000

    commit

---
+++ resources/views/profile/index.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.front')
+@section('title', '登録済みプロフィールの一覧')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <h2>プロフィール一覧</h2>
+        </div>
+        <div class="row">
+            <div class="list-profile col-md-12 mx-auto">
+                <div class="row">
+                    <table class="table table-dark">
+                        <thead>
+                            <tr>
+                                <th width="10%">名前</th>
+                                <th width="20%">性別</th>
+                                <th width="30%">趣味</th>
+                                <th width="30%">自己紹介</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($posts as $profile)
+                                <tr>
+                                    <td>{{ $profile->name }}</td>
+                                    <td>{{ $profile->gender }}</td>
+                                    <td>{{ $profile->hobby }}</td>
+                                    <td>{{ \Str::limit($profile->introduction, 100) }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+
